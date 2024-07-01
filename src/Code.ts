93c3946168f76bcaecaed9a6bbbd987a82984d91
Patricio Lopez J <lopezjuripatricio@gmail.com>
2024-07-01 15:09:21 -0400

    feat: Support array/range input =CHATGPT(A1:A4)

--- src/Code.ts
+++ src/Code.ts
@@ -1,7 +1,7 @@
 /**********************************************
  * @author Patricio López Juri
  * @license MIT
- * @version 1.0.0
+ * @version 1.1.0
  * @see https://github.com/urvana/appscript-chatgpt
  */
 
@@ -25,42 +25,34 @@ const OPENAI_API_KEY = "";
 const PROPERTY_KEY_OPENAPI = "OPENAI_API_KEY" as const;
 const MIME_JSON = "application/json" as const;
 
-/**
- * Custom function to call ChatGPT API
- *
- * @param {string} prompt The prompt to send to ChatGPT. Defaults to GT3.5 Turbo model.
- * @param {string} [model='gpt-3.5-turbo'] The model to use (e.g., 'gpt-3.5-turbo', 'gpt-4')
- * @param {number} [maxTokens=150] The maximum number of tokens to return
- * @return {string} The response from ChatGPT
- * @customfunction
- */
-function CHATGPT(
+type SpreadsheetInput<T> = T | Array<Array<T>>;
+
+function REQUEST(
+  apiKey: string,
+  promptSystem: string,
   prompt: string,
   model = "gpt-3.5-turbo",
   maxTokens = 150,
-): string {
-  const properties = PropertiesService.getUserProperties();
-  const apiKey = properties.getProperty(PROPERTY_KEY_OPENAPI) || OPENAI_API_KEY;
-  if (!apiKey) {
-    throw new Error(
-      'Use =CHATGPTKEY("YOUR_API_KEY") first. Get it from https://platform.openai.com/api-keys',
-    );
-  }
-
-  const processed = String(prompt || "").trim();
-  if (!processed) {
+) {
+  // Prepare user prompt
+  const cleaned = STRING_CLEAN(prompt);
+  if (cleaned === "") {
     return EMPTY;
   }
+  // Prepare system prompt
+  const cleanedSystem = STRING_CLEAN(promptSystem);
 
-  const url = "https://api.openai.com/v1/chat/completions";
+  // Compose messages.
+  const messages: ChatCompletionCreateParamsNonStreaming["messages"] = [];
+  if (cleanedSystem !== "") {
+    messages.push({ role: "system", content: cleanedSystem });
+  }
+  messages.push({ role: "user", content: cleaned });
 
   const payload: ChatCompletionCreateParamsNonStreaming = {
     model: model,
     max_tokens: maxTokens,
-    messages: [
-      { role: "system", content: SYSTEM_PROMPT.trim() },
-      { role: "user", content: processed },
-    ],
+    messages: messages,
   };
 
   const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
@@ -73,7 +65,10 @@ function CHATGPT(
     payload: JSON.stringify(payload),
   };
 
-  const response = UrlFetchApp.fetch(url, options);
+  const response = UrlFetchApp.fetch(
+    "https://api.openai.com/v1/chat/completions",
+    options,
+  );
   const json = response.getContentText();
   const data = JSON.parse(json) as ChatCompletion;
 
@@ -86,31 +81,69 @@ function CHATGPT(
 }
 
 /**
- * Custom function to call ChatGPT-3 API
+ * Custom function to call ChatGPT API
  *
- * @param {string} prompt The prompt to send to ChatGPT
+ * @param {string|Array<Array<string>>} prompt The prompt to send to ChatGPT. Defaults to GT3.5 Turbo model.
+ * @param {string} [model='gpt-3.5-turbo'] The model to use (e.g., 'gpt-3.5-turbo', 'gpt-4')
  * @param {number} [maxTokens=150] The maximum number of tokens to return
- * @return {string} The response from ChatGPT
+ * @return {string|Array<Array<string>>} The response from ChatGPT
  * @customfunction
  */
-function CHATGPT3(prompt: string, maxTokens = 150): string {
+function CHATGPT(
+  prompt: SpreadsheetInput<string>,
+  model = "gpt-3.5-turbo",
+  maxTokens = 150,
+): SpreadsheetInput<string> {
+  const properties = PropertiesService.getUserProperties();
+  const apiKey = properties.getProperty(PROPERTY_KEY_OPENAPI) || OPENAI_API_KEY;
+  if (!apiKey) {
+    throw new Error(
+      'Use =CHATGPTKEY("YOUR_API_KEY") first. Get it from https://platform.openai.com/api-keys',
+    );
+  }
+
+  if (Array.isArray(prompt)) {
+    return prompt.map((row) => {
+      return row.map((cell) => {
+        return REQUEST(apiKey, SYSTEM_PROMPT, cell, model, maxTokens);
+      });
+    });
+  }
+  return REQUEST(apiKey, SYSTEM_PROMPT, prompt, model, maxTokens);
+}
+
+/**
+ * Custom function to call ChatGPT-3 API
+ *
+ * @param {string|Array<Array<string>>} prompt The prompt to send to ChatGPT
+ * @param {number} [maxTokens=150] The maximum number of tokens to return
+ * @return {string|Array<Array<string>>} The response from ChatGPT
+ * @customfunction
+ */
+function CHATGPT3(
+  prompt: SpreadsheetInput<string>,
+  maxTokens = 150,
+): SpreadsheetInput<string> {
   return CHATGPT(prompt, "gpt-3.5-turbo", maxTokens);
 }
 
 /**
  * Custom function to call ChatGPT-4 API
  *
- * @param {string} prompt The prompt to send to ChatGPT
+ * @param {string|Array<Array<string>>} prompt The prompt to send to ChatGPT
  * @param {number} [maxTokens=150] The maximum number of tokens to return
- * @return {string} The response from ChatGPT
+ * @return {string|Array<Array<string>>} The response from ChatGPT
  * @customfunction
  */
-function CHATGPT4(prompt: string, maxTokens = 150): string {
+function CHATGPT4(
+  prompt: SpreadsheetInput<string>,
+  maxTokens = 150,
+): SpreadsheetInput<string> {
   return CHATGPT(prompt, "gpt-4", maxTokens);
 }
 
 /**
- * Custom function to set the OpenAI API key
+ * Custom function to set the OpenAI API key. Get yours at: https://platform.openai.com/api-keys
  *
  * @param {string} apiKey The OpenAI API key to save
  * @return {string} Confirmation message
@@ -120,4 +153,14 @@ function CHATGPTKEY(apiKey: string): string {
   const properties = PropertiesService.getUserProperties();
   properties.setProperty("OPENAI_API_KEY", apiKey);
   return "✅ Remove this cell";
+}
+
+function STRING_CLEAN(value: string | number): string {
+  if (value === undefined || value === null) {
+    return "";
+  }
+  if (typeof value === "number") {
+    return value.toString();
+  }
+  return String(value).trim();
 }
